d2daa191868ea839ce82a7ace03a1063eb7baafc
nikballal <nikhil.ballal1994@gmail.com>
2022-06-30 15:34:04 +1000

    front end only

---
+++ src/context/AppReducer.js
@@ -0,0 +1,20 @@
+//reducer is used to specify the state changes in response to certain actions to the context
+
+export default (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+    default:
+      return state;
+  }
+};
